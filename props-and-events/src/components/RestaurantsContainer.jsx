ad037f229836b9e7a9a233c64d1717cb6ee1cc04
Danny McCarragher <dannymccarragher@gmail.com>
2025-04-13 20:40:49 -0700

    Completed Mapping data array

--- props-and-events/src/components/RestaurantsContainer.jsx
+++ props-and-events/src/components/RestaurantsContainer.jsx
@@ -1,7 +1,12 @@
+import Restaurant from "./Restaurant.jsx";
+import data from '../data/data.js';
+
 function RestaurantsContainer() {
   return (
     <div className="restaurantContainer">
 
+      {data.map((restaurant) => ( <Restaurant restaurant={restaurant} key={restaurant.id}/>))}    
+      
     </div>
   );
 }
